a39082b1fe770979dafbce9e460cc747b5da4f1a
BravoJH <hejunalala@126.com>
2015-05-01 12:54:49 +0800

    findMe提交百度SDK简易使用代码

---
+++ findMe/src/com/bravo/findme/MapActivity.java
@@ -0,0 +1,96 @@
+package com.bravo.findme;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+
+public class MapActivity extends Activity {
+	@SuppressWarnings("unused")
+	private static final String LTAG = MapActivity.class.getSimpleName();
+	private MapView mMapView;
+	private BaiduMap mBaiduMap;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext  
+        //注意该方法要再setContentView方法之前实现  
+        SDKInitializer.initialize(getApplicationContext());  
+        setContentView(R.layout.activity_map);  
+      //获取地图控件引用  
+        mMapView = (MapView) findViewById(R.id.bmapView);  
+/*		Intent intent = getIntent();
+		if (intent.hasExtra("x") && intent.hasExtra("y")) {
+			// 当用intent参数时，设置中心点为指定点
+			Bundle b = intent.getExtras();
+			LatLng p = new LatLng(b.getDouble("y"), b.getDouble("x"));
+			mMapView = new MapView(this,
+					new BaiduMapOptions().mapStatus(new MapStatus.Builder()
+							.target(p).build()));
+		} else {
+			mMapView = new MapView(this, new BaiduMapOptions());
+		}
+		setContentView(mMapView);
+		mBaiduMap = mMapView.getMap();
+		
+		// 注册 SDK 广播监听者
+		IntentFilter iFilter = new IntentFilter();
+		iFilter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR);
+		iFilter.addAction(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR);
+		mReceiver = new SDKReceiver();
+		registerReceiver(mReceiver, iFilter);*/
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// activity 暂停时同时暂停地图控件
+		mMapView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// activity 恢复时同时恢复地图控件
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// activity 销毁时同时销毁地图控件
+		mMapView.onDestroy();
+	}
+	
+	/**
+	 * 构造广播监听类，监听 SDK key 验证以及网络异常广播
+	 */
+	public class SDKReceiver extends BroadcastReceiver {
+		public void onReceive(Context context, Intent intent) {
+			String s = intent.getAction();
+			Log.d(LTAG, "action: " + s);
+			if (s.equals(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)) {
+				System.out.println("key 验证出错! 请在 AndroidManifest.xml 文件中检查 key 设置");
+			} else if (s
+					.equals(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR)) {
+				System.out.println("网络出错");
+			}
+		}
+	}
+
+	private SDKReceiver mReceiver;
+
+}
